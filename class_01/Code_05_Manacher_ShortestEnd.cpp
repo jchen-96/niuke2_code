9a9862dd03932cff638aa422df4b2eff2548cf5d
304jc <304jc@qq.com>
2018-08-06 18:34:47 +0800

    daily commit

--- class_01/Code_05_Manacher_ShortestEnd.cpp
+++ class_01/Code_05_Manacher_ShortestEnd.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-class Code_05_Manacher_01
+class Code_05_Manacher_ShortestEnd
 {
   public:
     static string getString(string s)
@@ -17,37 +17,64 @@ class Code_05_Manacher_01
         int index = 0;
         for (int i = 0; i < res.size(); i++)
         {
-            res[i]=(i&1)==0?'#':s[index++];
+            res[i] = (i & 1) == 0 ? '#' : s[index++];
         }
         return res;
     }
 
-    static int Manahcer(string s){
-        if(s.empty()){
-            return -1;
+    static string Manahcer(string s)
+    {
+        if (s.empty())
+        {
+            return "";
         }
-        string str=getString(s);
-        int right=-1;
-        int core=-1;
+        string str = getString(s);
+        int right = -1;
+        int core = -1;
         int pR[str.size()];
-        for(int i=0;i<str.size();i++){
-            pR[i]=right>i?min(pR[core*2-i],right-i):1;
-            while(i+pR[i]<str.size()&&i-pR[i]>-1){
-                if(str[i+pR[i]]==str[i-pR[i]]){
+        int res = -1;
+        for (int i = 0; i < str.size(); i++)
+        {
+            pR[i] = right > i ? min(pR[core * 2 - i], right - i) : 1;
+            while (i + pR[i] < str.size() && i - pR[i] > -1)
+            {
+                if (str[i + pR[i]] == str[i - pR[i]])
+                {
                     pR[i]++;
-                }else{
+                }
+                else
+                {
                     break;
                 }
             }
-            if(i+pR[i]>right){
-                right=i+pR[i];
-                core=i;
+            if (i + pR[i] > right)
+            {
+                right = i + pR[i];
+                core = i;
             }
-            if(right=str.size()){
-                return -1;//undo
+            if (right == str.size())
+            {
+                res = core;
+                break;
             }
         }
-        return -1;
-
+        cout << res << endl;
+        for (int i = res - pR[res]; i >= 0; i--)
+        {
+            str += str[i];
+        }
+        string f_res = "";
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (str[i] != '#')
+                f_res += str[i];
+        }
+        return f_res;
     }
 };
+int main(int argc, char const *argv[])
+{
+    cout << Code_05_Manacher_ShortestEnd::Manahcer("abcd123321") << endl;
+    getchar();
+    return 0;
+}
