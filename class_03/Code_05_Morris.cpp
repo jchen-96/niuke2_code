c0e10030ba857e965527825705f040623b7a78a3
304jc <304jc@qq.com>
2018-08-11 13:50:23 +0800

    dialy comit

--- class_03/Code_05_Morris.cpp
+++ class_03/Code_05_Morris.cpp
@@ -7,3 +7,81 @@
  *          mostright 的右指针为空，mostright->right=cur, cur=cur.left
  *          mostright 的右指针为==cur，mostright-ritht=null,cur=cur.right
  **/
+
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+class Node
+{
+  public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int v) : value(v), left(NULL), right(NULL) {}
+};
+
+class Code_05_Morris
+{
+  public:
+    static void MorrisPre(Node *root)
+    {
+        if (root == NULL)
+            return;
+        Node *cur = root;
+        Node *mostRight = NULL;
+        while (cur != NULL)
+        {
+            mostRight = cur->left;
+            if (mostRight != NULL)
+            {
+                while (mostRight != NULL && mostRight->right != NULL)
+                    mostRight = mostRight->right;
+                if (mostRight->right == NULL)
+                {
+                    mostRight->right = cur;
+                    cout << cur->value << " ";
+                    cur = cur->left;
+                    continue;
+                }
+                else
+                {
+                    mostRight->right = NULL;
+                }
+            }
+            else
+                cout << cur->value << " ";
+            cur = cur->right;
+        }
+    }
+    static void MorrisIn(Node *root)
+    {
+        if (root == NULL)
+            return;
+        Node *cur = root;
+        Node *mostRight = NULL;
+        while (cur != NULL)
+        {
+            mostRight = cur->left;
+            if (mostRight != NULL)
+            {
+                while (mostRight != NULL && mostRight->right != NULL)
+                    mostRight = mostRight->right;
+                if (mostRight->right == NULL)
+                {
+                    mostRight->right = cur;
+                    cur = cur->left;
+                    continue;
+                }
+                else
+                    mostRight->right = NULL;
+            }
+
+            cout << cur->value << " ";
+            cur = cur->right;
+        }
+    }
+};
